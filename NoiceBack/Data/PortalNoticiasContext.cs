ffc976345dc255b212c210a02dc789a5fd4c2cba
SAGAZ <messie.sagaz1990@gmail.com>
2026-09-09 12:40:55 -0600

    Fase 2.4 - sincronizar eventos con MySQL

--- NoiceBack/Data/PortalNoticiasContext.cs
+++ NoiceBack/Data/PortalNoticiasContext.cs
@@ -333,28 +333,74 @@ namespace PortalNoticiasAPI.Data
             modelBuilder.Entity<Evento>(e =>
             {
                 e.ToTable("Eventos");
+
                 e.HasKey(x => x.IdEvento);
-                e.Property(x => x.IdEvento).HasColumnName("idEvento");
-                e.Property(x => x.Titulo).HasColumnName("Titulo").IsRequired();
-                e.Property(x => x.Descripcion).HasColumnName("Descripcion");
-                e.Property(x => x.Lugar).HasColumnName("Lugar");
-                e.Property(x => x.FechaInicio).HasColumnName("FechaInicio");
-                e.Property(x => x.FechaFin).HasColumnName("FechaFin");
-                e.Property(x => x.HoraInicio).HasColumnName("HoraInicio");
-                e.Property(x => x.HoraFin).HasColumnName("HoraFin");
-                e.Property(x => x.IdUsuario).HasColumnName("idUsuario");
-                e.Property(x => x.IdCategoria).HasColumnName("idCategoria");
-                e.Property(x => x.FechaAlta).HasColumnName("FechaAlta");
-                e.Property(x => x.Activo).HasColumnName("Activo");
+
+                e.Property(x => x.IdEvento)
+                    .HasColumnName("idEvento")
+                    .ValueGeneratedOnAdd();
+
+                e.Property(x => x.Titulo)
+                    .HasColumnName("Titulo")
+                    .HasMaxLength(200)
+                    .IsRequired();
+
+                e.Property(x => x.Descripcion)
+                    .HasColumnName("Descripcion")
+                    .HasColumnType("text");
+
+                e.Property(x => x.Lugar)
+                    .HasColumnName("Lugar")
+                    .HasMaxLength(200);
+
+                e.Property(x => x.FechaInicio)
+                    .HasColumnName("FechaInicio")
+                    .HasColumnType("date")
+                    .IsRequired();
+
+                e.Property(x => x.FechaFin)
+                    .HasColumnName("FechaFin")
+                    .HasColumnType("date")
+                    .IsRequired();
+
+                e.Property(x => x.HoraInicio)
+                    .HasColumnName("HoraInicio")
+                    .HasColumnType("time");
+
+                e.Property(x => x.HoraFin)
+                    .HasColumnName("HoraFin")
+                    .HasColumnType("time");
+
+                e.Property(x => x.IdUsuario)
+                    .HasColumnName("idUsuario")
+                    .IsRequired();
+
+                e.Property(x => x.IdCategoria)
+                    .HasColumnName("idCategoria")
+                    .IsRequired(false);
+
+                e.Property(x => x.FechaAlta)
+                    .HasColumnName("FechaAlta")
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                e.Property(x => x.Activo)
+                    .HasColumnName("Activo")
+                    .HasColumnType("bit(1)")
+                    .HasDefaultValue(true);
 
                 e.HasOne(x => x.Usuario)
-                 .WithMany(u => u.Eventos)
-                 .HasForeignKey(x => x.IdUsuario);
+                    .WithMany(u => u.Eventos)
+                    .HasForeignKey(x => x.IdUsuario)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("fk_eventos_usuario");
 
                 e.HasOne(x => x.Categoria)
-                 .WithMany(c => c.Eventos)
-                 .HasForeignKey(x => x.IdCategoria)
-                 .IsRequired(false);
+                    .WithMany(c => c.Eventos)
+                    .HasForeignKey(x => x.IdCategoria)
+                    .IsRequired(false)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("fk_eventos_categoria");
             });
 
             // ---------- Academia ----------
